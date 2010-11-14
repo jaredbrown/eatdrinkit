01dbaacb561e20a063ef7721a7caecf2a850b337
Jared Brown <jaredbrown@gmail.com>
2010-11-14 12:04:36 -0500

    Style Failed login message

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -14,8 +14,9 @@ class SessionController < ApplicationController
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
       redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
+      flash[:notice] = "Logged in"
     else
+      flash[:notice] = 'Login failed'
       render :action => 'new'
     end
   end
