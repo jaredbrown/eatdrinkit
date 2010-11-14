85ddb4297f7719f6d4ee51ef49bc1dd801ea8b46
eatyourcarrots <anthony@sticksnleaves.com>
2010-11-14 15:28:33 -0500

    Fixed issue when someone tries to checkin the same location multiple times from the same screen.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -43,21 +43,21 @@ class PlacesController < ApplicationController
       shout = 'Arrived at ' + @venue['name'] + ' using EatDrink.it (http://eatdrink.it).' 
       request_token = session[:request_token]
       access_token = request_token.get_access_token(:oauth_verifier => params['oauth_verifier'])
-      access_token.post('http://api.foursquare.com/v1/checkin.json', { :vid => params[:id], :facebook => '1', :twitter => '1', :shout => shout })
-    else
-      consumer = OAuth::Consumer.new(ENV['oauth_key'], ENV['oauth_secret'], {
-        :site => 'http://foursquare.com',
-        :scheme => :header,
-        :http_method => :post,
-        :request_token_path => '/oauth/request_token',
-        :access_token_path => '/oauth/access_token',
-        :authorize_path => '/mobile/oauth/authenticate'
-      })
-      
-      request_token = consumer.get_request_token(:oauth_callback => request.env['REQUEST_URI'])
-      
-      session[:request_token] = request_token
+      access_token.post('http://api.foursquare.com/v1/checkin.json', { :vid => params[:id], :facebook => '1', :twitter => '1', :shout => shout })  
     end
+    
+    consumer = OAuth::Consumer.new(ENV['oauth_key'], ENV['oauth_secret'], {
+      :site => 'http://foursquare.com',
+      :scheme => :header,
+      :http_method => :post,
+      :request_token_path => '/oauth/request_token',
+      :access_token_path => '/oauth/access_token',
+      :authorize_path => '/mobile/oauth/authenticate'
+    })
+      
+    request_token = consumer.get_request_token(:oauth_callback => request.env['REQUEST_URI'])
+      
+    session[:request_token] = request_token
     
     @foursquare_url = request_token.authorize_url
     @all_reviews = Review.find_all_by_venue_id(params[:id])
